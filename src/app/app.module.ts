28486e51b170b62ce181edcb3f7323bf64e83fed
8e3yn4uk <sashko.kondratenko@gmail.com>
2019-08-05 01:13:16 +0300

    components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,44 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { FeedbackComponent } from './feedback/feedback.component';
+import { NotesComponent } from './notes/notes.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import {Router, RouterModule, Routes} from "@angular/router";
+
+const appRouts :Routes = [
+  {
+    path:'notes',
+    component:NotesComponent
+  },
+  {
+    path:'feedback',
+    component:FeedbackComponent
+  },
+  {
+    path:'',
+    component:NotesComponent,
+    pathMatch:'full'
+  },
+  {
+    path:'**',
+    component:NotFoundComponent
+  }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    FeedbackComponent,
+    NotesComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRouts, {enableTracing:true})
   ],
   providers: [],
   bootstrap: [AppComponent]
